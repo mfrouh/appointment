d64d6eeb0266cdb0b8abc830fdc88366a97a7c3c
mohamed frouh <mohamedfrouh96@gmail.com>
2021-01-10 16:54:38 +0200

    add backend layout

---
+++ resources/views/layouts/main-sidebar.blade.php
@@ -0,0 +1,31 @@
+<!-- main-sidebar -->
+		<div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+		<aside class="app-sidebar sidebar-scroll">
+			<div class="main-sidebar-header active">
+                @php
+                    $setting=App\Models\Setting::first();
+                @endphp
+				<a class="desktop-logo logo-light active" href="{{ url('/') }}"><img src="{{URL::asset($setting->logo)}}" class="main-logo" alt="logo"></a>
+				<a class="desktop-logo logo-dark active" href="{{ url('/') }}"><img src="{{URL::asset('assets/img/brand/logo-white.png')}}" class="main-logo dark-theme" alt="logo"></a>
+				<a class="logo-icon mobile-logo icon-light active" href="{{ url('/') }}"><img src="{{URL::asset('assets/img/brand/favicon.png')}}" class="logo-icon" alt="logo"></a>
+				<a class="logo-icon mobile-logo icon-dark active" href="{{ url('/') }}"><img src="{{URL::asset('assets/img/brand/favicon-white.png')}}" class="logo-icon dark-theme" alt="logo"></a>
+			</div>
+			<div class="main-sidemenu">
+				<div class="app-sidebar__user clearfix">
+					<div class="dropdown user-pro-body">
+						<div class="">
+							<img class="avatar avatar-xl brround" src="{{URL::asset(auth()->user()->image)}}"><span class="avatar-status profile-status bg-green"></span>
+						</div>
+						<div class="user-info">
+							<h4 class="font-weight-semibold mt-3 mb-0">{{auth()->user()->name}}</h4>
+						</div>
+					</div>
+				</div>
+				<ul class="side-menu">
+					<li class="slide">
+						<a class="side-menu__item" href="{{ url('/dashboard' ) }}"><span class="side-menu__label">الرئيسية</a>
+                    </li>
+				</ul>
+			</div>
+		</aside>
+<!-- main-sidebar -->
